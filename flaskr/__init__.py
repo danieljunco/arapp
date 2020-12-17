b387155f0766201ac656f8b09872b0116a2d70c4
Daniel Junco <daniel.junco>
2020-12-17 15:29:26 +0100

    add flask cors

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
     
     @app.route('/')
     def hello():
