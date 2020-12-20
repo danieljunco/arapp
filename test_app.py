84309199f0f9301a7d17e0f037b8d80534bf5515
Daniel Junco <daniel.junco>
2020-12-20 18:13:11 +0100

    set up first test for item_types endpoint

--- test_app.py
+++ test_app.py
@@ -30,13 +30,22 @@ class ArarInventoryTestCase(unittest.TestCase):
         """Executed after react test"""
         pass
 
+    def test_create_new_item_type(self):
+        res = self.client().post('/items/types', json=self.new_item_type)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['id'])
+        self.assertEqual(data['name'], self.new_item_type['name'])
+        self.assertTrue(data['created_at'])
+
     def test_get_paginated_item_types(self):
         res = self.client().get('/items/types')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['pagination'])
-        self.assertTrue(len(data['items']))
+        self.assertTrue(data['items'])
 
 
 # Make the test conveniently executable
