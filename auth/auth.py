816ff3a73f63b0a4f000cc056343c0ddf73f3fd3
Daniel Junco <daniel.junco>
2020-12-22 02:33:28 +0100

    test authentication env variables

--- auth/auth.py
+++ auth/auth.py
@@ -6,9 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = os.environ["AUTH0_DOMAIN"]
-ALGORITHMS = os.environ["ALGORITHMS"]
-API_AUDIENCE = os.environ["API_AUDIENCE"]
+AUTH0_DOMAIN = "arar.eu.auth0.com"
+ALGORITHMS = ["RS256"]
+API_AUDIENCE = "inventory"
 
 ## AuthError Exception
 '''
