d85420ee3b0ed911efc80cfdb5ec8ab85e1ab323
Daniel Junco <daniel.junco>
2020-12-22 16:37:56 +0100

    make use of env variables in auth

--- auth/auth.py
+++ auth/auth.py
@@ -6,13 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = "arar.eu.auth0.com"
-ALGORITHMS = ["RS256"]
-API_AUDIENCE = "inventory"
-
-# AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-# ALGORITHMS = [os.environ.get('ALGORITHMS')]
-# API_AUDIENCE = os.environ.get('API_AUDIENCE')
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = [os.environ.get('ALGORITHMS')]
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 ## AuthError Exception
 '''
